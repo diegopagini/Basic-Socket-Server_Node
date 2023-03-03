3c9bbc2648b65adf74c5f6ba79c59f550a05557c
Diego Pagini <diego.pagini@neoris.com>
2023-03-03 13:32:50 +0100

    cors

--- models/server.js
+++ models/server.js
@@ -5,6 +5,7 @@ const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
 const Sockets = require('./sockets');
+const cors = require('cors');
 
 class Server {
 	constructor() {
@@ -19,6 +20,8 @@ class Server {
 	middlewares() {
 		// Public directory.
 		this.app.use(express.static(path.resolve(__dirname, '../public')));
+		// CORS.
+		this.app.use(cors());
 	}
 
 	sockets() {
