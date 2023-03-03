4234018c3bf4b014c0d1c43364979bdbc47887c7
Diego Pagini <diego.pagini@neoris.com>
2023-03-03 12:10:24 +0100

    dotenv

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /** @format */
+require('dotenv').config();
 
 const Server = require('./models/server');
 
 const server = new Server();
+
 server.execute();
--- models/server.js
+++ models/server.js
@@ -9,7 +9,7 @@ const Sockets = require('./sockets');
 class Server {
 	constructor() {
 		this.app = express();
-		this.port = 8080;
+		this.port = process.env.PORT;
 		// Http Server.
 		this.server = http.createServer(this.app);
 		// Sockets configuration.
